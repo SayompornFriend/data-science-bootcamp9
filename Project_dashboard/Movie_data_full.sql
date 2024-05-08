34e635fa647b970e00924976dde5ef89138fe6c8
SayompornFriend <167858794+SayompornFriend@users.noreply.github.com>
2024-05-08 20:13:33 +0700

    Create Movie_data_full.sql

---
+++ Project_dashboard/Movie_data_full.sql
@@ -0,0 +1,13 @@
+--Join table movie,moviedirector,franchise,moviegenre
+SELECT 
+	*,
+	f.franchisename,
+    g.Genre,
+    d.DirectorName
+    --t.ActorName
+FROM movie m
+LEFT JOIN franchise f on m.FranchiseID = f.FranchiseId
+left join moviegenre g on m.MovieID = g.MovieGenreID
+LEFT join moviedirector d on m.MovieID = d.MovieID
+--LEFT join topbilledcast t on m.MovieID = t.MovieID
+;
